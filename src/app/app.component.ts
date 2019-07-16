8749784192d978d91a2f14d082ecf8ba3500d5be
deito <deito.alvarez@gmail.com>
2019-07-16 12:46:24 -0500

    utilizando typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  /*
   title = 'fazt-angular6-app';
   users = ['ryan', 'joe', 'cameron', 'john', 'bruce'];
   activated = false;
+  */
+ name: string = 'Ryan Ray';
+ age: number;
+ address: {
+   street: string;
+   city: string;
+ };
+ hobbies: string[];
+
+ constructor(){
+   this.age = 28;
+   this.address = {
+     street: '221B Baker Street',
+     city: 'London'
+   };
+   this.hobbies =  ['swimming', 'read', 'write'];
+ }
 }
